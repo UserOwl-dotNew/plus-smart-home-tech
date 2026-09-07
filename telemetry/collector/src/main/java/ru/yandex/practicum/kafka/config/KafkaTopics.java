a8925c064f15348c46e23aff08a152c8f8daa709
owl <dmitrikojevnikov@yandex.ru>
2026-09-07 09:02:17 +0300

    feat: add topic - telemetry.snapshots.v1

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/config/KafkaTopics.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/config/KafkaTopics.java
@@ -3,4 +3,5 @@ package ru.yandex.practicum.kafka.config;
 public class KafkaTopics {
     public static final String TELEMETRY_SENSORS_V1 = "telemetry.sensors.v1";
     public static final String TELEMETRY_HUBS_V1 = "telemetry.hubs.v1";
+    public static final String TELEMETRY_SNAPSHOTS_V1 = "telemetry.snapshots.v1";
 }
