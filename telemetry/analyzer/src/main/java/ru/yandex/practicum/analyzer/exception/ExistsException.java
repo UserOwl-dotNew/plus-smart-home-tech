e2af2c25c1975b3ec76615fe54e4170d19e9c6c0
owl <dmitrikojevnikov@yandex.ru>
2026-09-12 16:05:18 +0300

    feat: add exceptions

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/exception/ExistsException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.analyzer.exception;
+
+public class ExistsException extends RuntimeException {
+    public ExistsException(String message) {
+        super(message);
+    }
+}
